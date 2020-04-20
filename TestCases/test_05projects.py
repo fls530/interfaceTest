8e7f6e19544ad13ac9995616c3c5945a6b563654
fls530 <530740300@qq.com>
2020-04-20 22:26:49 +0800

    4月20日更新 优化创建新项目用例

--- TestCases/test_05projects.py
+++ TestCases/test_05projects.py
@@ -7,7 +7,7 @@ from Common.handle_excel import HandleExcel
 from Common.handle_config import conf
 from Common.handle_db import db
 from Common.handle_logging import log
-from Common.handle_data import EnvData, replace_data,login
+from Common.handle_data import EnvData, replace_data, login
 from Common.handle_assert import assert_dict
 
 filename = os.path.join(DATA_DIR, "apitest.xlsx")
